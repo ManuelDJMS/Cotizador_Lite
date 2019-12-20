604cd7d464c1f346d5d23d2a0bed9605dea459da
PaauRF <paaurodriiguez@gmail.com>
2019-12-20 12:13:46 -0600

    cotizacion por articulo

---
+++ Presentacion/FrmCotizacionPorArticulo.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmCotizacionPorArticulo
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmCotizacionPorArticulo))
+        Me.GunaElipse1 = New Guna.UI.WinForms.GunaElipse(Me.components)
+        Me.PanelFormularios = New System.Windows.Forms.Panel()
+        Me.PanelContenedor = New System.Windows.Forms.Panel()
+        Me.GunaSeparator1 = New Guna.UI.WinForms.GunaSeparator()
+        Me.Label26 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GunaLabel2 = New Guna.UI.WinForms.GunaLabel()
+        Me.PanelBarraTitulo = New Guna.UI.WinForms.GunaPanel()
+        Me.GunaPictureBox2 = New Guna.UI.WinForms.GunaPictureBox()
+        Me.BtnRestaurar = New Guna.UI.WinForms.GunaImageButton()
+        Me.BtnCerrar = New Guna.UI.WinForms.GunaImageButton()
+        Me.GunaVSeparator2 = New Guna.UI.WinForms.GunaVSeparator()
+        Me.LbLogin = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaPictureBox1 = New Guna.UI.WinForms.GunaPictureBox()
+        Me.GunaVSeparator1 = New Guna.UI.WinForms.GunaVSeparator()
+        Me.BtnMaximizar = New Guna.UI.WinForms.GunaImageButton()
+        Me.BtnMinimizar = New Guna.UI.WinForms.GunaImageButton()
+        Me.GunaImageButton4 = New Guna.UI.WinForms.GunaImageButton()
+        Me.PanelFormularios.SuspendLayout()
+        Me.PanelContenedor.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelBarraTitulo.SuspendLayout()
+        CType(Me.GunaPictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GunaElipse1
+        '
+        Me.GunaElipse1.TargetControl = Me
+        '
+        'PanelFormularios
+        '
+        Me.PanelFormularios.BackColor = System.Drawing.Color.White
+        Me.PanelFormularios.Controls.Add(Me.PanelContenedor)
+        Me.PanelFormularios.Controls.Add(Me.PanelBarraTitulo)
+        Me.PanelFormularios.Location = New System.Drawing.Point(-1, -1)
+        Me.PanelFormularios.Name = "PanelFormularios"
+        Me.PanelFormularios.Size = New System.Drawing.Size(1160, 625)
+        Me.PanelFormularios.TabIndex = 0
+        '
+        'PanelContenedor
+        '
+        Me.PanelContenedor.Controls.Add(Me.GunaSeparator1)
+        Me.PanelContenedor.Controls.Add(Me.Label26)
+        Me.PanelContenedor.Controls.Add(Me.PictureBox1)
+        Me.PanelContenedor.Controls.Add(Me.GunaLabel2)
+        Me.PanelContenedor.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelContenedor.Location = New System.Drawing.Point(0, 50)
+        Me.PanelContenedor.Name = "PanelContenedor"
+        Me.PanelContenedor.Size = New System.Drawing.Size(1160, 575)
+        Me.PanelContenedor.TabIndex = 6
+        '
+        'GunaSeparator1
+        '
+        Me.GunaSeparator1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaSeparator1.LineColor = System.Drawing.Color.Silver
+        Me.GunaSeparator1.Location = New System.Drawing.Point(8, 57)
+        Me.GunaSeparator1.Name = "GunaSeparator1"
+        Me.GunaSeparator1.Size = New System.Drawing.Size(1140, 10)
+        Me.GunaSeparator1.TabIndex = 167
+        '
+        'Label26
+        '
+        Me.Label26.AutoSize = True
+        Me.Label26.Enabled = False
+        Me.Label26.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label26.Location = New System.Drawing.Point(87, 38)
+        Me.Label26.Name = "Label26"
+        Me.Label26.Size = New System.Drawing.Size(213, 18)
+        Me.Label26.TabIndex = 166
+        Me.Label26.Text = "Exportar Cotizaciones 2019 a 2020"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 6)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(66, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 52
+        Me.PictureBox1.TabStop = False
+        '
+        'GunaLabel2
+        '
+        Me.GunaLabel2.AutoSize = True
+        Me.GunaLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.GunaLabel2.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GunaLabel2.ForeColor = System.Drawing.Color.ForestGreen
+        Me.GunaLabel2.Location = New System.Drawing.Point(84, 6)
+        Me.GunaLabel2.Name = "GunaLabel2"
+        Me.GunaLabel2.Size = New System.Drawing.Size(282, 32)
+        Me.GunaLabel2.TabIndex = 51
+        Me.GunaLabel2.Text = "Cotización por Artículo"
+        '
+        'PanelBarraTitulo
+        '
+        Me.PanelBarraTitulo.BackColor = System.Drawing.Color.White
+        Me.PanelBarraTitulo.Controls.Add(Me.GunaPictureBox2)
+        Me.PanelBarraTitulo.Controls.Add(Me.BtnRestaurar)
+        Me.PanelBarraTitulo.Controls.Add(Me.BtnCerrar)
+        Me.PanelBarraTitulo.Controls.Add(Me.GunaVSeparator2)
+        Me.PanelBarraTitulo.Controls.Add(Me.LbLogin)
+        Me.PanelBarraTitulo.Controls.Add(Me.GunaPictureBox1)
+        Me.PanelBarraTitulo.Controls.Add(Me.GunaVSeparator1)
+        Me.PanelBarraTitulo.Controls.Add(Me.BtnMaximizar)
+        Me.PanelBarraTitulo.Controls.Add(Me.BtnMinimizar)
+        Me.PanelBarraTitulo.Controls.Add(Me.GunaImageButton4)
+        Me.PanelBarraTitulo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelBarraTitulo.Location = New System.Drawing.Point(0, 0)
+        Me.PanelBarraTitulo.Name = "PanelBarraTitulo"
+        Me.PanelBarraTitulo.Size = New System.Drawing.Size(1160, 50)
+        Me.PanelBarraTitulo.TabIndex = 5
+        '
+        'GunaPictureBox2
+        '
+        Me.GunaPictureBox2.BaseColor = System.Drawing.Color.White
+        Me.GunaPictureBox2.Image = Global.Cotizador_Lite.My.Resources.Resources.papel
+        Me.GunaPictureBox2.Location = New System.Drawing.Point(12, 8)
+        Me.GunaPictureBox2.Name = "GunaPictureBox2"
+        Me.GunaPictureBox2.Size = New System.Drawing.Size(32, 32)
+        Me.GunaPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.GunaPictureBox2.TabIndex = 9
+        Me.GunaPictureBox2.TabStop = False
+        '
+        'BtnRestaurar
+        '
+        Me.BtnRestaurar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnRestaurar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnRestaurar.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.BtnRestaurar.Image = Global.Cotizador_Lite.My.Resources.Resources.icons8_enlarge_48
+        Me.BtnRestaurar.ImageSize = New System.Drawing.Size(16, 16)
+        Me.BtnRestaurar.Location = New System.Drawing.Point(1083, 8)
+        Me.BtnRestaurar.Name = "BtnRestaurar"
+        Me.BtnRestaurar.OnHoverImage = Nothing
+        Me.BtnRestaurar.OnHoverImageOffset = New System.Drawing.Point(0, -1)
+        Me.BtnRestaurar.Size = New System.Drawing.Size(32, 32)
+        Me.BtnRestaurar.TabIndex = 8
+        Me.BtnRestaurar.Visible = False
+        '
+        'BtnCerrar
+        '
+        Me.BtnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnCerrar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnCerrar.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.BtnCerrar.Image = Global.Cotizador_Lite.My.Resources.Resources.icons8_multiply_48
+        Me.BtnCerrar.ImageSize = New System.Drawing.Size(16, 16)
+        Me.BtnCerrar.Location = New System.Drawing.Point(1121, 8)
+        Me.BtnCerrar.Name = "BtnCerrar"
+        Me.BtnCerrar.OnHoverImage = Nothing
+        Me.BtnCerrar.OnHoverImageOffset = New System.Drawing.Point(0, -1)
+        Me.BtnCerrar.Size = New System.Drawing.Size(32, 32)
+        Me.BtnCerrar.TabIndex = 7
+        '
+        'GunaVSeparator2
+        '
+        Me.GunaVSeparator2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaVSeparator2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GunaVSeparator2.LineColor = System.Drawing.Color.WhiteSmoke
+        Me.GunaVSeparator2.Location = New System.Drawing.Point(799, 12)
+        Me.GunaVSeparator2.Name = "GunaVSeparator2"
+        Me.GunaVSeparator2.Size = New System.Drawing.Size(10, 25)
+        Me.GunaVSeparator2.TabIndex = 6
+        '
+        'LbLogin
+        '
+        Me.LbLogin.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LbLogin.AutoSize = True
+        Me.LbLogin.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbLogin.Location = New System.Drawing.Point(872, 17)
+        Me.LbLogin.Name = "LbLogin"
+        Me.LbLogin.Size = New System.Drawing.Size(65, 20)
+        Me.LbLogin.TabIndex = 5
+        Me.LbLogin.Text = "Manuel"
+        '
+        'GunaPictureBox1
+        '
+        Me.GunaPictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaPictureBox1.BaseColor = System.Drawing.Color.White
+        Me.GunaPictureBox1.Image = Global.Cotizador_Lite.My.Resources.Resources.icons8_verified_account_28
+        Me.GunaPictureBox1.Location = New System.Drawing.Point(825, 8)
+        Me.GunaPictureBox1.Name = "GunaPictureBox1"
+        Me.GunaPictureBox1.Size = New System.Drawing.Size(32, 32)
+        Me.GunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.GunaPictureBox1.TabIndex = 4
+        Me.GunaPictureBox1.TabStop = False
+        '
+        'GunaVSeparator1
+        '
+        Me.GunaVSeparator1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaVSeparator1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GunaVSeparator1.LineColor = System.Drawing.Color.WhiteSmoke
+        Me.GunaVSeparator1.Location = New System.Drawing.Point(1035, 12)
+        Me.GunaVSeparator1.Name = "GunaVSeparator1"
+        Me.GunaVSeparator1.Size = New System.Drawing.Size(10, 25)
+        Me.GunaVSeparator1.TabIndex = 3
+        '
+        'BtnMaximizar
+        '
+        Me.BtnMaximizar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnMaximizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnMaximizar.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.BtnMaximizar.Image = Global.Cotizador_Lite.My.Resources.Resources.icons8_full_screen_32
+        Me.BtnMaximizar.ImageSize = New System.Drawing.Size(16, 16)
+        Me.BtnMaximizar.Location = New System.Drawing.Point(1083, 8)
+        Me.BtnMaximizar.Name = "BtnMaximizar"
+        Me.BtnMaximizar.OnHoverImage = Nothing
+        Me.BtnMaximizar.OnHoverImageOffset = New System.Drawing.Point(0, -1)
+        Me.BtnMaximizar.Size = New System.Drawing.Size(32, 32)
+        Me.BtnMaximizar.TabIndex = 2
+        '
+        'BtnMinimizar
+        '
+        Me.BtnMinimizar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnMinimizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnMinimizar.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.BtnMinimizar.Image = Global.Cotizador_Lite.My.Resources.Resources.icons8_horizontal_line_48
+        Me.BtnMinimizar.ImageSize = New System.Drawing.Size(16, 16)
+        Me.BtnMinimizar.Location = New System.Drawing.Point(1045, 8)
+        Me.BtnMinimizar.Name = "BtnMinimizar"
+        Me.BtnMinimizar.OnHoverImage = Nothing
+        Me.BtnMinimizar.OnHoverImageOffset = New System.Drawing.Point(0, -1)
+        Me.BtnMinimizar.Size = New System.Drawing.Size(32, 32)
+        Me.BtnMinimizar.TabIndex = 1
+        '
+        'GunaImageButton4
+        '
+        Me.GunaImageButton4.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.GunaImageButton4.Image = Nothing
+        Me.GunaImageButton4.ImageSize = New System.Drawing.Size(16, 16)
+        Me.GunaImageButton4.Location = New System.Drawing.Point(8, 8)
+        Me.GunaImageButton4.Name = "GunaImageButton4"
+        Me.GunaImageButton4.OnHoverImage = Nothing
+        Me.GunaImageButton4.OnHoverImageOffset = New System.Drawing.Point(0, -1)
+        Me.GunaImageButton4.Size = New System.Drawing.Size(32, 32)
+        Me.GunaImageButton4.TabIndex = 0
+        '
+        'FrmCotizacionPorArticulo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1160, 625)
+        Me.Controls.Add(Me.PanelFormularios)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "FrmCotizacionPorArticulo"
+        Me.Text = "FrmCotizacionPorArticulo"
+        Me.PanelFormularios.ResumeLayout(False)
+        Me.PanelContenedor.ResumeLayout(False)
+        Me.PanelContenedor.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelBarraTitulo.ResumeLayout(False)
+        Me.PanelBarraTitulo.PerformLayout()
+        CType(Me.GunaPictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GunaElipse1 As Guna.UI.WinForms.GunaElipse
+    Friend WithEvents PanelFormularios As Panel
+    Friend WithEvents PanelBarraTitulo As Guna.UI.WinForms.GunaPanel
+    Friend WithEvents GunaPictureBox2 As Guna.UI.WinForms.GunaPictureBox
+    Friend WithEvents BtnRestaurar As Guna.UI.WinForms.GunaImageButton
+    Friend WithEvents BtnCerrar As Guna.UI.WinForms.GunaImageButton
+    Friend WithEvents GunaVSeparator2 As Guna.UI.WinForms.GunaVSeparator
+    Friend WithEvents LbLogin As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GunaPictureBox1 As Guna.UI.WinForms.GunaPictureBox
+    Friend WithEvents GunaVSeparator1 As Guna.UI.WinForms.GunaVSeparator
+    Friend WithEvents BtnMaximizar As Guna.UI.WinForms.GunaImageButton
+    Friend WithEvents BtnMinimizar As Guna.UI.WinForms.GunaImageButton
+    Friend WithEvents GunaImageButton4 As Guna.UI.WinForms.GunaImageButton
+    Friend WithEvents PanelContenedor As Panel
+    Friend WithEvents GunaSeparator1 As Guna.UI.WinForms.GunaSeparator
+    Friend WithEvents Label26 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GunaLabel2 As Guna.UI.WinForms.GunaLabel
+End Class
---
+++ Presentacion/FrmCotizacionPorArticulo.vb
@@ -0,0 +1,90 @@
+﻿Imports System.Runtime.InteropServices
+Public Class FrmCotizacionPorArticulo
+    Dim banm, banb As Boolean
+    Dim clave1 As String
+#Region "FUNCIONALIDADES DEL FORMULARIO"
+    'RESIZE DEL FORMULARIO- CAMBIAR TAMAÑO
+    Dim cGrip As Integer = 10
+
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        If (m.Msg = 132) Then
+            Dim pos As Point = New Point((m.LParam.ToInt32 And 65535), (m.LParam.ToInt32 + 16))
+            pos = Me.PointToClient(pos)
+            If ((pos.X _
+                        >= (Me.ClientSize.Width - cGrip)) _
+                        AndAlso (pos.Y _
+                        >= (Me.ClientSize.Height - cGrip))) Then
+                m.Result = CType(17, IntPtr)
+                Return
+            End If
+        End If
+        MyBase.WndProc(m)
+    End Sub
+    '----------------DIBUJAR RECTANGULO / EXCLUIR ESQUINA PANEL 
+    Dim sizeGripRectangle As Rectangle
+    Dim tolerance As Integer = 15
+
+    Protected Overrides Sub OnSizeChanged(ByVal e As EventArgs)
+        MyBase.OnSizeChanged(e)
+        Dim region = New Region(New Rectangle(0, 0, Me.ClientRectangle.Width, Me.ClientRectangle.Height))
+        sizeGripRectangle = New Rectangle((Me.ClientRectangle.Width - tolerance), (Me.ClientRectangle.Height - tolerance), tolerance, tolerance)
+        region.Exclude(sizeGripRectangle)
+        Me.PanelContenedor.Region = region
+        Me.Invalidate()
+    End Sub
+
+    '----------------COLOR Y GRIP DE RECTANGULO INFERIOR
+    Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
+        Dim blueBrush As SolidBrush = New SolidBrush(Color.FromArgb(255, 255, 255))
+        e.Graphics.FillRectangle(blueBrush, sizeGripRectangle)
+        MyBase.OnPaint(e)
+        ControlPaint.DrawSizeGrip(e.Graphics, Color.Transparent, sizeGripRectangle)
+    End Sub
+    'ARRASTRAR EL FORMULARIO
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(ByVal hWnd As System.IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer)
+    End Sub
+
+    Private Sub PanelBarraTitulo_MouseMove(sender As Object, e As MouseEventArgs) Handles PanelBarraTitulo.MouseMove
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+    Dim lx, ly As Integer
+
+    Private Sub BtnCerrar_Click(sender As Object, e As EventArgs) Handles BtnCerrar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub BtnMaximizar_Click(sender As Object, e As EventArgs) Handles BtnMaximizar.Click
+        Me.WindowState = FormWindowState.Normal
+        lx = Me.Location.X
+        ly = Me.Location.Y
+        sw = Me.Size.Width
+        sh = Me.Size.Height
+        BtnMaximizar.Visible = False
+        BtnRestaurar.Visible = True
+        Me.Size = Screen.PrimaryScreen.WorkingArea.Size
+        Me.Location = Screen.PrimaryScreen.WorkingArea.Location
+    End Sub
+
+    Private Sub BtnRestaurar_Click(sender As Object, e As EventArgs) Handles BtnRestaurar.Click
+        Me.WindowState = FormWindowState.Normal
+        Me.Size = New Size(sw, sh)
+        Me.Location = New Point(lx, ly)
+        BtnMaximizar.Visible = True
+        BtnRestaurar.Visible = False
+    End Sub
+
+
+
+    Private Sub BtnMinimizar_Click(sender As Object, e As EventArgs) Handles BtnMinimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Dim sw, sh As Integer
+#End Region
+End Class
